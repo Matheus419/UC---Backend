610513c21aaf6120c0699d76a7df07e9fb3b7c4f
Godoy <mhenriquebp@gmail.com>
2024-11-26 21:35:17 -0300

    Subindo Backend

---
+++ app.js
@@ -0,0 +1,14 @@
+import { get } from 'http';
+import carros2024 from './tabelacarros.js';
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (requisicao, resposta) => {
+    resposta.status(200).send(carros2024);
+});
+
+
+app.listen(3000,() => console.log("Servidor rodando com sucesso"));
---
+++ tabelacarros.js
@@ -0,0 +1,20 @@
+const carros2024 = [
+    {
+        nome: 'Ferrari',
+        sigla: 'FER',
+        velocidadeMaxima: 340,
+        potencia: 500,
+        acelerecao: 2.9,
+        preco: 300000,
+    },
+    {
+        nome: 'Lamborghini',
+        sigla: 'LAM',
+        velocidadeMaxima: 355,
+        potencia: 570,
+        acelerecao: 2.9,
+        preco: 330000,
+    },
+];
+
+export default carros2024;
